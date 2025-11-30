9fe85af82eb2d14c483d85daa513bc0d19917242
EdnizLeandro <147220920+EdnizLeandro@users.noreply.github.com>
2025-11-30 09:58:12 -0300

    Delete Código site Jobin2/src directory
    1

--- Código site Jobin2/src/lib/lumi.ts
+++
@@ -1,9 +0,0 @@
-
-import { createClient } from '@lumi.new/sdk'
-
-export const lumi = createClient({
-  projectId: 'p356137871667519488',
-  apiBaseUrl: 'https://api.lumi.new',
-  authOrigin: 'https://auth.lumi.new',
-})
-
